9795f2c426b39ac19604fc858cb72ca1d93aa39d
quyhuynh49 <quyhuynh49@gmail.com>
2023-09-19 12:01:36 +0700

    first commit

---
+++ src/components/NewReminder.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export default function NewReminder({
+  onAddReminder,
+}: {
+  onAddReminder: (title: string) => void;
+}): JSX.Element {
+  const [title, setTitle] = useState("");
+  const submitForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title) return;
+    onAddReminder(title);
+    setTitle("");
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="title"></label>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        id="title"
+        type="text"
+        className="form-control"
+      />
+      <button type="submit" className="btn btn-primary my-3 rounded-pill">
+        Add Reminder
+      </button>
+    </form>
+  );
+}
